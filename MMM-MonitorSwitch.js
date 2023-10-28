e29a3a84413d7d14e18bcd53a51caa96a82728d2
vshakhlin <vshakhlin@gmail.com>
2023-10-29 04:17:27 +0800

    base implement

---
+++ MMM-MonitorSwitch.js
@@ -0,0 +1,32 @@
+﻿Module.register('MMM-PIR-Sensor',{
+	requiresVersion: '2.1.0',
+	defaults: {
+
+	},
+
+	userPresence: false,
+
+	// Override socket notification handler.
+	/*socketNotificationReceived: function (notification, payload) {
+		if (notification === 'USER_PRESENCE') {
+			this.userPresence = payload;
+			this.sendNotification(notification, payload);
+			if  (payload === false && this.config.powerSavingNotification === true){
+				this.sendNotification("SHOW_ALERT",{type:"notification", message:this.config.powerSavingMessage});
+			}
+			this.updateDom();
+		} else if (notification === 'SHOW_ALERT') {
+			this.sendNotification(notification, payload)
+		}
+	},
+
+	notificationReceived: function (notification, payload) {
+		if (notification === 'SCREEN_WAKEUP') {
+			this.sendNotification(notification, payload)
+		}
+	},*/
+
+	start: function () {
+		Log.info('Starting module: ' + this.name);
+	}
+});
---
+++ node_helper.js
@@ -0,0 +1,24 @@
+/* Magic Mirror Node Helper: MMM-MP3Player
+ * By asimhsidd
+ *
+ * Remade by Pavel Smelov.
+ * Version 1.2.0 - 2020.09.14
+ * GPLv3 License
+ */
+
+const NodeHelper = require("node_helper");
+const exec = require('child_process').exec;
+
+module.exports = NodeHelper.create({
+	socketNotificationReceived: function(notification, payload) {
+		var self = this;
+		switch(notification) {
+			case "MONITOR_ON":
+				exec("/usr/bin/vcgencmd display_power 1", null);
+				break;
+			case "MONITOR_OFF":
+				exec("/usr/bin/vcgencmd display_power 0", null);
+				break;
+		}
+	},
+});
